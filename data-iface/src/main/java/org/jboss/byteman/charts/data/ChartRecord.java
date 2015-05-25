206633708b65aa5bbfb5c6d7accd112a5f373769
akashche <akashche@redhat.com>
2015-05-25 18:01:03 +0100

    initial commit

---
+++ data-iface/src/main/java/org/jboss/byteman/charts/data/ChartRecord.java
@@ -0,0 +1,112 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2010 Red Hat and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+
+package org.jboss.byteman.charts.data;
+
+import java.util.LinkedHashMap;
+
+import static org.jboss.byteman.charts.utils.CollectionUtils.arrayToMap;
+import static org.jboss.byteman.charts.utils.StringUtils.defaultString;
+
+/**
+ * Data structure for records collected in application and used to display the chart
+ *
+ * @author akashche
+ * Date: 5/25/15
+ */
+public class ChartRecord {
+    private long ts;
+    private String marker;
+    private LinkedHashMap<String, Object> data;
+
+    /**
+     * Private constructor for JSON tools
+     */
+    private ChartRecord() {
+    }
+
+    /**
+     * Constructor
+     *
+     * @param ts timestamp
+     * @param marker marker string
+     * @param data data related to this record
+     */
+    public ChartRecord(long ts, String marker, LinkedHashMap<String, Object> data) {
+        this.ts = ts;
+        this.marker = defaultString(marker);
+        this.data = data;
+    }
+
+    /**
+     * Constructor with varargs support
+     *
+     * @param ts timestamp
+     * @param marker marker string
+     * @param dataFields data related to this record in the form of
+     *                   "key", "value", "key", "value" syntax
+     */
+    public ChartRecord(long ts, String marker, Object... dataFields) {
+        this.ts = ts;
+        this.marker = defaultString(marker);
+        this.data = arrayToMap(dataFields);
+    }
+
+    /**
+     * Timestamp accessor
+     *
+     * @return ts field
+     */
+    public long getTs() {
+        return ts;
+    }
+
+    /**
+     * Marker accessor
+     *
+     * @return  market field
+     */
+    public String getMarker() {
+        return marker;
+    }
+
+    /**
+     * Data accessor
+     *
+     * @return data field
+     */
+    public LinkedHashMap<String, Object> getData() {
+        return data;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "ChartRecord{" +
+                "ts=" + ts +
+                ", marker='" + marker + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
